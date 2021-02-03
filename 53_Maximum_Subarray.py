a0e760ff3d5a3aaa209435147a432f99db5e0248
ashmitya <anna.bronytska@gmail.com>
2021-02-03 12:49:59 -0500

    53 done

---
+++ 53_Maximum_Subarray.py
@@ -0,0 +1,10 @@
+def maxSubArray(nums):
+    current_sum = max_sum = nums[0]
+    for i in range(len(nums)):
+        current_sum = max(nums[i], current_sum + nums[i])
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+
+
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
