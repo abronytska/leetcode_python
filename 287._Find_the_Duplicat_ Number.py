0c0ed23b16112742209c99d9c18cd28525e6a9e5
ashmitya <anna.bronytska@gmail.com>
2021-09-16 15:46:07 -0400

    287

---
+++ 287._Find_the_Duplicat_ Number.py
@@ -0,0 +1,22 @@
+def findDuplicate(nums):
+    dic = {n: 0 for n in set(nums)}
+    for i in range(len(nums)):
+        dic[nums[i]] += 1
+        if dic[nums[i]] > 1:
+            return nums[i]
+
+
+nums = [0, 2, 3, 3, 4]
+print(findDuplicate(nums))
+
+
+nums = [0, 1, 4, 4]
+print(findDuplicate(nums))
+
+
+nums = [6, 4, 2, 1, 4, 3, 5]
+print(findDuplicate(nums))
+
+
+nums = [1, 3, 4, 2, 2]
+print(findDuplicate(nums))
