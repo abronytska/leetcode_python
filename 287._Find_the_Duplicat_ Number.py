81372221b87c14328989f00c78631b7fb590a667
ashmitya <anna.bronytska@gmail.com>
2021-09-16 16:05:14 -0400

    287 one more solution

--- 287._Find_the_Duplicat_ Number.py
+++ 287._Find_the_Duplicat_ Number.py
@@ -1,10 +1,15 @@
 def findDuplicate(nums):
-    dic = {n: 0 for n in set(nums)}
-    for i in range(len(nums)):
-        dic[nums[i]] += 1
-        if dic[nums[i]] > 1:
-            return nums[i]
+    # dic = {n: 0 for n in set(nums)}
+    # for i in range(len(nums)):
+    #     dic[nums[i]] += 1
+    #     if dic[nums[i]] > 1:
+    #         return nums[i]
 
+    dic = {}
+    for i in range(len(nums)):
+        dic[nums[i]] = dic.get(nums[i], 0) + 1
+        if dic[nums[i]] > 1:
+            return nums[i] 
 
 nums = [0, 2, 3, 3, 4]
 print(findDuplicate(nums))
